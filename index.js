2114603a7d204ad2a26685a6d7437982227cf79a
bhaskar <ashwanibhaskar05@gmail.com>
2024-12-27 17:12:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("perimeter-finder")
+let a = prompt("enter base")
+let b = prompt("enter height")
+ 
+if(1/2 * a * b){
+    alert(1/2 * a * b)
+}
+else{
+    alert("enter the correct number")
+}
